e8326362838fcb9e74df448cbebf002099aefc9b
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2024-06-17 21:03:59 +0200

    Use wip rules_python Gazelle PR #1976

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from dateutil.easter import easter
+from dateutil.easter import EASTER_WESTERN
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World! Easter will next occur on '%s'" % easter(2025, EASTER_WESTERN)
 
 if __name__ == "__main__":
     app.run()
